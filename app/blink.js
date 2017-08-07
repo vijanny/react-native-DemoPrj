280384411fd9e3855d1913263a5872d606bae6bf
vijanny <vijanny@126.com>
2017-08-07 14:06:12 +0800

    完成主页header

--- app/blink.js
+++ app/blink.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {View, AppRegistry, StyleSheet, Text,Image,Dimensions} from 'react-native';
+import {View, AppRegistry, StyleSheet, Text,Image,Dimensions,TouchableHighlight   } from 'react-native';
 import {Left, Body, Right,Container,Title} from 'native-base';
 import Header from './header';
 
@@ -25,18 +25,41 @@ export default class Blink extends Component {
        };  
     // 每1000毫秒对showText状态做一次取反操作  
   }  
+  _onPress(){
+    this.props.navigation.navigate('DrawerOpen');
+  }
 
   render() {  
-
     // 根据当前showText的值决定是否显示text内容  
     let display = this.state.showText ? this.state.name : '我一直在闪';  
     return (     
         <View style = {styles.container}>
-          <View style = {styles.header}>
-            <Image 
-            style={styles.headerIcon}
-            source={require('../img/menu.png')}/>
-          </View>
+          <View style={{backgroundColor:"white",flexDirection:"row",height:headerHeight}}>  
+              <View style={{flex:1,flexDirection:'column',justifyContent:'center',paddingLeft:10}}>  
+                <TouchableHighlight   
+                    onPress={this._onPress.bind(this)}
+                    style={styles.headerIcon}
+                    underlayColor = '#F5F5F5'
+                  >
+                  <View >
+                      <Image 
+                      style={styles.headerIcon}
+                      source={require('../img/menu.png')}
+                      />
+                  </View>
+                </TouchableHighlight>              
+              </View>  
+              <View style={{flex:1,flexDirection:'column',justifyContent:'center'}}>  
+                <View style = {{flexDirection:'row',alignItems:'center',justifyContent:'center'}}>
+                    <Text style={{fontSize:20,color:'#F08A78',fontWeight:'bold'}} >主页</Text>                    
+                </View>
+              </View>  
+              <View style={{flex:1,flexDirection:'column',justifyContent:'center'}}>  
+                <View style = {{flexDirection:'row',alignItems:'center',justifyContent:'flex-end',paddingRight:10}}>
+                   
+                </View>
+              </View>  
+          </View>  
           <View style={styles.content}>
             <Text>DFADFAS</Text>
           </View>
@@ -45,19 +68,33 @@ export default class Blink extends Component {
   }  
 }  
 
+
+
 const styles = StyleSheet.create({
   red: {
     color: 'red',
     fontSize:28
   },
-
+  headerOutline:{
+    flex:1,
+    width:width,
+    height:headerHeight,
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+    borderWidth:2,
+    borderColor:'#000'
+  },
   header:{
     width:width,
     height:headerHeight,
     backgroundColor:'white',
-    justifyContent:'center',
-    paddingLeft:20,
-    paddingRight:20,
+    justifyContent:'space-between',
+    flexDirection:'row',
+    paddingLeft:10,
+    paddingRight:10,
+    borderWidth:2,
+    borderColor:'red'
   },
   headerIcon:{
     width:30,
