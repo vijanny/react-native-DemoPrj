c925ffa7546732c71e79d556f8db9ed2063610d9
vijanny <vijanny@126.com>
2017-08-10 12:03:29 +0800

    完成主页 item按键

--- app/blink.js
+++ app/blink.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import {View, AppRegistry, StyleSheet, Text,Image,Dimensions,TouchableHighlight   } from 'react-native';
+import {View, AppRegistry, StyleSheet, Text,Image,Dimensions,TouchableHighlight,Alert,Animated,Easing,} from 'react-native';
 import PercentageCircle from 'react-native-percentage-circle';
-
 import Header from './header';
+import SinWave from './SinWave';
 
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 const headerHeight = 50;
-
+const screenHeight = height;
+const screenWidth  = width;
 export default class Blink extends Component {  
     static navigationOptions = {
     drawerLabel: '主页',
@@ -18,20 +19,27 @@ export default class Blink extends Component {
         style={[styles.icon, {tintColor: tintColor}]}
       />
     ),
-  };
+  }
   constructor(props) {  
     super(props);  
     this.state = { 
-      showText: false,
+      milkButtonOn:true,
+      waterButtonOn:false,
        };  
     // 每1000毫秒对showText状态做一次取反操作  
   }  
+
   _onPress(){
     this.props.navigation.navigate('DrawerOpen');
   }
-
-  render() {  
-    // 根据当前showText的值决定是否显示text内容  
+  _milkOnPress(){
+    this.setState({milkButtonOn:true,waterButtonOn:false});
+  }
+  _waterOnPress(){
+    this.setState({milkButtonOn:false,waterButtonOn:true});
+  }
+  render() {
+    const animatedStyle = { height: this.animatedHeightValue, width: this.animatedWidthValue }
     return (     
         <View style = {styles.container}>
           <Header 
@@ -43,25 +51,42 @@ export default class Blink extends Component {
           backgroundColor='white'
           />
           <View style={styles.content}>
-               <View  style ={{flexDirection:'row',justifyContent:'center'}}>
-                <Image style ={{width:60,height:60}} source ={require('../img/decrease.png')}/>
-                <PercentageCircle radius={80} percent={100} color={"#F08A78"}  innerColor='#F9DDD2' borderWidth = {10}>
-                  <Text></Text>
-                </PercentageCircle>
-                <Image style ={{width:60,height:60}} source ={require('../img/add.png')}/>  
+              <View style={styles.operateItem}>
+                <TouchableHighlight style={[styles.operateItemButton,this.state.milkButtonOn?{backgroundColor:'#F08A78'}:null]} onPress={this._milkOnPress.bind(this)} >
+                  <View style={styles.operateItemButtonInner}>
+                    {this.state.milkButtonOn?<Image style={{width:20,height:20}} source={require('../img/dot.png')}/>:<Text></Text>}
+                    <Text style={styles.operateItemText}>冲奶</Text>  
+                    <Text></Text>               
+                  </View>
+                </TouchableHighlight>
+                <TouchableHighlight style={[styles.operateItemButton,this.state.waterButtonOn?{backgroundColor:'#F08A78'}:null]} onPress={this._waterOnPress.bind(this)}>
+                  <View style={styles.operateItemButtonInner}>
+                    {this.state.waterButtonOn?<Image style={{width:20,height:20}} source={require('../img/dot.png')}/>:<Text></Text>}
+                    <Text style={styles.operateItemText}>喝水</Text>  
+                    <Text></Text>               
+                  </View>
+                </TouchableHighlight>
               </View>
-              <View  >
-                <PercentageCircle radius={80} percent={100} color={"#F08A78"}  innerColor='#F9DDD2' borderWidth = {5}>
-                  <Text></Text>
-                </PercentageCircle>  
+               <View  style ={styles.operatePanel}>
+                  <View style={styles.operatePanelCommon}>
+                      <Image style ={styles.operatePanelIcon} source ={require('../img/decrease.png')}/>
+                  </View>
+                  <View style={styles.operatePanelCommon}>
+                      <PercentageCircle radius={90} percent={100} color={"#F08A78"}  innerColor='#F9DDD2' borderWidth = {20}>
+                            <Text></Text>                  
+                      </PercentageCircle>
+                  </View>
+                  <View style={styles.operatePanelCommon}>
+                      <Image style ={styles.operatePanelIcon} source ={require('../img/add.png')}/>
+                  </View>
               </View>
-              <View>
-                <Text> 213213213</Text>
-              </View>
-              <View>
-                <Text> 213213213</Text>
-              </View>
-                            
+              <TouchableHighlight style={styles.operateButton}  underlayColor='#F9DDD2' >
+                <View>
+                <Text style={[{color:'#fff',fontSize:28}]}>
+                  一键冲奶
+                </Text>
+                </View>
+              </TouchableHighlight>
           </View>
         </View>
     );  
@@ -69,36 +94,14 @@ export default class Blink extends Component {
 }  
 
 
-
 const styles = StyleSheet.create({
-  red: {
-    color: 'red',
-    fontSize:28
-  },
-  headerOutline:{
-    flex:1,
-    width:width,
-    height:headerHeight,
-    flexDirection:'column',
-    justifyContent:'center',
-    alignItems:'center',
-    borderWidth:2,
-    borderColor:'#000'
-  },
-  header:{
-    width:width,
-    height:headerHeight,
-    backgroundColor:'white',
-    justifyContent:'space-between',
-    flexDirection:'row',
-    paddingLeft:10,
-    paddingRight:10,
-    borderWidth:2,
-    borderColor:'red'
-  },
-  headerIcon:{
-    width:30,
-    height:30
+  box: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#c93939',
+    height: 100,
+    width: 100,
+    overflow: 'hidden'
   },
   container:{
     height:height,
@@ -110,10 +113,62 @@ const styles = StyleSheet.create({
     alignItems:'center',
     justifyContent:'space-around'
   },
+  operateItem:{
+    width:width*60/100,
+    height:40,
+    borderWidth:2,
+    borderColor:'white',
+    borderRadius:30,
+    flexDirection:'row',
+    justifyContent:'space-around',
+    paddingLeft:15,
+    paddingRight:15
+  },
+  operateItemButton:{
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+    borderWidth:0,
+    borderColor:'red',
+    borderRadius:30,
+    width:(width*60/100)*55/100
+  },
+  operateItemButtonInner:{
+    flexDirection:'row',
+    justifyContent:'space-around',
+    width:(width*60/100)*55/100
+  },
+  operateItemText:{
+    color:'white',
+    fontSize:18
+  },
+  operatePanel:{
+    width:width,
+    flexDirection:'row',
+    justifyContent:'center',
+  },
+  operatePanelIcon:{
+    width:40,
+    height:40
+  },
+  operatePanelCommon:{
+    marginLeft:10,
+    marginRight:10,
+    flexDirection:'column',
+    justifyContent:'center'
+  },
+  operateButton:{
+    width:width*85/100,
+    borderWidth:2,
+    borderColor:'#fff',
+    borderRadius:45,
+    alignItems:'center',  
+    justifyContent: 'center', 
+    backgroundColor:'#F08A78'
+  },
   icon: {
     width: 24,
     height: 24,
   },
 });
-
 
